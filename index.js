8fd2663884c3332f855b315dac476a081ff58f92
Chase V <glacieralz@gmail.com>
2022-01-31 17:57:12 -0600

    part 2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateimage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,6 +20,16 @@ client.on("messageCreate", (message) => {
     if(message.content == "hi"){
         message.reply("Hello World!")
     }
+})
+
+const welcomeChannelId = "937846480348282910"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content:`<@${member.id}> Welcome to the Server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
